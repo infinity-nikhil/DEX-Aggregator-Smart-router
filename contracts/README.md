5118e15c8a1e7176f70076d5540f1c58c595ac3f
Nikhil Kumar <nikhil950833@gmail.com>
2026-08-25 21:29:54 +0000

    fee-logic

--- contracts/README.md
+++ contracts/README.md
@@ -1,3 +1,7 @@
 # IMPORTANT
 Ofc we are using the forge 
 And one important msg chat is that i my self don't know how to make so will research on it and will keep updating the repo as we find something new ...for now it is just a owner transfer contract and it's test file
+
+### fee setup logic 
+Guys lets be honest it's 3 am in the morning and i am really confused in understanding the test file logic so for now it's just the main contract will upload the test file logic later. 
+But i did learn something about the what is an internal function (it is the main challenge in testing btw). So, basically it is function you can't directly call by the frontend abi or test. Why so ? Because it get's called inside a function, like when you call an external function then inside that function this internal function gets triggered. That is it ... well read the commit i am going to sleep ...BYE!!!
--- contracts/src/Aggregator.sol
+++ contracts/src/Aggregator.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Aggregator {
     address public owner;
     address public pendingOwner;
+    address public feeWallet;
+
+    uint16 public feeBps;
+    uint16 public constant HARD_CAP = 100;
+
+    IERC20 public immutable usdg;
 
     error NotOwner();
     error NotPendingOwner();
+    error FeeTooHigh();
 
-    constructor() {
+    constructor(address _usdg) {
         owner = msg.sender;
+        feeWallet = msg.sender;
+        usdg = IERC20(_usdg); 
     }
 
     modifier onlyOwner() {
@@ -23,7 +34,25 @@ contract Aggregator {
 
     function acceptOwnership() external {
         if (msg.sender != pendingOwner) revert NotPendingOwner();
+
         owner = pendingOwner;
         pendingOwner = address(0);
+    }
+
+    function setFee(uint16 feeBps_) external onlyOwner{
+        if (feeBps_ > HARD_CAP) revert FeeTooHigh();
+
+        feeBps = feeBps_;
+    }
+
+    function _split(uint256 usdgLeg) internal view returns (uint256 fee, uint256 rest) {
+        fee = (usdgLeg * feeBps) / 10_000;
+        rest = usdgLeg - fee;
+    }
+
+    function _skim(uint256 fee) internal {
+        if (fee > 0) {
+            usdg.safeTransfer(feeWallet, fee);
+        }
     }
 }
